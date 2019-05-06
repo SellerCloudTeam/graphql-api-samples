8328ac8c0e9fa6265b72977ccfe522b907641bd7
Stan <stanimir@sellercloud.com>
2019-05-06 18:05:45 +0300

    Server deployment configuration

--- src/js/src/config.js
+++ src/js/src/config.js
@@ -1,7 +1,3 @@
-
-// TODO: Enter your SellerCloud server ID or API server URL here
-//       e.g. https://<server-id>.api.sellercloud.com/gql
-export const GRAPHQL_SERVER = your_graphql_url;
-
+export const GRAPHQL_SERVER = "/gql";
 export const GRAPHQL_ENDPOINT = "/graphql";
 export const TOKEN_ENDPOINT = "/token";
